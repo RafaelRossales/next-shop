14723bbd50fe17094bcc77030ca577205ab808c7
RafaelRossales <rapha2704@gmail.com>
2025-07-20 19:32:46 -0300

    feat: update Header styles to enhance layout and button design

--- src/styles/pages/styles.ts
+++ src/styles/pages/styles.ts
@@ -13,5 +13,31 @@ export const Header = styled("header", {
   maxWidth: 1180,
   margin: "0 auto",
   padding: "2rem 0",
-  background: "red",
+
+  display: "flex",
+  justifyContent: "space-between",
+
+  ".cart-btn": {
+    backgroundColor: "$gray800",
+    border: 0,
+    padding: "1.1rem",
+    borderRadius: 8,
+    cursor: "pointer",
+  },
+
+  span: {
+    backgroundColor: "$green300",
+    borderRadius: "50%",
+    height: "30px",
+    width: "30px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+    border: "4px solid $gray900",
+    color: "white",
+    position: "absolute",
+    marginLeft: "2.5rem",
+    top: 25,
+  },
 });
